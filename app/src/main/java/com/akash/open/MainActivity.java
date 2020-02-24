6be58faf33a04faaa2ae0676c2b81b998dec5276
akash2237778 <akash2237778@gmail.com>
2020-02-24 19:18:56 +0530

    Nav created

---
+++ app/src/main/java/com/akash/open/MainActivity.java
@@ -0,0 +1,20 @@
+package com.akash.open;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
