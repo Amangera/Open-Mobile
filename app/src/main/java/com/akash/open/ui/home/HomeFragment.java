6da91e99bf6d2eb7a510213df4be1ef3789885c3
akash2237778 <akash2237778@gmail.com>
2020-05-06 23:19:23 +0530

    Failed Image Slider

--- app/src/main/java/com/akash/open/ui/home/HomeFragment.java
+++ app/src/main/java/com/akash/open/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.akash.open.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,23 +14,88 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.akash.open.R;
+import com.akash.open.SliderAdapterExample;
+import com.akash.open.SliderItem;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    SliderView sliderView;
+
+    private SliderAdapterExample adapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.nav_home_fragment, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView2 = root.findViewById(R.id.text_home2);
+
+
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                textView.setText("In Association with :");
+                textView2.setText("AWARE | ADOPT | CONTRIBUTE");
             }
         });
+
+
+       // sliderView = sliderView.findViewById(R.id.imageSlider);
+      //  adapter = new SliderAdapterExample(getContext());
+     //   renewItems();
+       // sliderView.setSliderAdapter(adapter);
+
+       // sliderView.setIndicatorAnimation(IndicatorAnimations.THIN_WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+       // sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+       // sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
+       // sliderView.setIndicatorSelectedColor(Color.WHITE);
+       // sliderView.setIndicatorUnselectedColor(Color.GRAY);
+       // sliderView.setScrollTimeInSec(3);
+       // sliderView.setAutoCycle(false);
+
+
         return root;
+    }
+
+    public void renewItems() {
+        List<SliderItem> sliderItemList = new ArrayList<>();
+        //dummy data
+        for (int i = 0; i < 5; i++) {
+            SliderItem sliderItem = new SliderItem();
+            sliderItem.setDescription("Slider Item " + i);
+            if (i % 2 == 0) {
+                sliderItem.setImageUrl("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+            } else {
+                sliderItem.setImageUrl("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260");
+            }
+            sliderItemList.add(sliderItem);
+        }
+        adapter.renewItems(sliderItemList);
+    }
+
+
+
+    public void removeLastItem(View view) {
+        if (adapter.getCount() - 1 >= 0)
+            adapter.deleteItem(adapter.getCount() - 1);
+    }
+
+
+    public void addNewItem(View view) {
+        SliderItem sliderItem = new SliderItem();
+        sliderItem.setDescription("Slider Item Added Manually");
+        sliderItem.setImageUrl("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+        adapter.addItem(sliderItem);
     }
 }
